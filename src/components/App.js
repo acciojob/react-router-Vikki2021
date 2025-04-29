ff2bca36ac0761a1075473f027a2217aa5f5ac57
Vikas Kumar <vk972832@gmail.com>
2025-04-29 23:31:17 +0530

    job-card update

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,20 @@
-
 import React from "react";
 import './../styles/App.css';
+import Home from "./Home";
+import About from "./About";
+import Navigation from "./Navigation";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 const App = () => {
   return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
+    <BrowserRouter>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App
